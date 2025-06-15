9c87e267305ce0a46b8b7ac8a56c4ff43627d116
BeatDiaz <beatdiazmusic@gmail.com>
2025-06-15 11:58:13 +0300

    Creat Terrorist class

---
+++ Entities/Terrorist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investigation.Models
+{
+    public class Terrorist
+    {
+        string Name { get; set; }
+
+        Sensor[] Sensors = new Sensor[AudioSensor, AudioSensor];
+        string[] Weaknesses = new string[2];
+
+    }
+}
