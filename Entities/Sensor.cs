fea7943243b5ac4a5d26a53cb59fa0ddf5bdb4cb
BeatDiaz <beatdiazmusic@gmail.com>
2025-06-15 11:25:03 +0300

    Creat sensor class

---
+++ Entities/Sensor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investigation.Models
+{
+    public class Sensor
+    {
+        string Name;
+
+        static public int Activat(string[] sensors, string[] weaknesses)
+        {
+            int counterActivates = 1;
+            foreach (var sensor in sensors)
+            {
+                if (weaknesses.Contains(sensor))
+                {
+                    counterActivates++;
+                }
+            }
+            return counterActivates;
+        }
+    }
+}
