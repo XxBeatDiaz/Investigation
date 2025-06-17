2e6b3b4b8df8003f6270b3531fc3708df7086837
BeatDiaz <beatdiazmusic@gmail.com>
2025-06-17 18:41:31 +0300

    Creat sensor factory

---
+++ Factories/SensorFactory.cs
@@ -0,0 +1,22 @@
+﻿using Investigation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investigation.Models
+{
+    public class SensorFactory 
+    {
+        static public Sensor CreateSensor(int type)
+        {
+            if (type == 1) return new AudioSensor();
+            if (type == 2) return new ThermalSensor();
+            if (type == 3) return new PulseSensor();
+
+            else return new Sensor();
+        }
+    }
+}
+
